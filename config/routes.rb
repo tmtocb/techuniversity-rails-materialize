22a92f656631ea3f5991c5d4a76563597a6cca73
tmtocb <kittz.esport@gmail.com>
2021-03-02 18:26:03 +0100

    add Materialize sticky footer & contact page, follow TDD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,4 +15,18 @@ describe PagesController do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET contact" do
+    subject { get :contact }
+
+    it "renders the contact template" do
+      subject
+      expect(response).to render_template(:contact)
+    end
+
+    it "returns http success response" do
+      subject
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
